eb639f9f0eb4d6a8da944281da3168f86fd71c6e
Maarten Sebregts <msebregts@ignitioncomputing.com>
2023-08-11 12:08:54 +0200

    Refactoring and more docstrings for autodoc.py

--- docs/sphinx_dd_extension/autodoc.py
+++ docs/sphinx_dd_extension/autodoc.py
@@ -7,74 +7,59 @@ from textwrap import dedent, indent
 from typing import Any, Dict, List
 from xml.etree import ElementTree
 
-from docutils import nodes
-from docutils.statemachine import StringList
 from sphinx.application import Sphinx
-from sphinx.util.docutils import SphinxDirective
-from sphinx.util.nodes import nested_parse_with_titles
 from sphinx.util import logging
 
 logger = logging.getLogger(__name__)
 
 
+# Utilities that are documented separately to reduce the size of the reference pages
 DOCUMENTED_UTILITIES = ["ids_properties", "code"]
+# Indentation character
+INDENT = " "
 
 
-class DDAutoDoc(SphinxDirective):
-    final_argument_whitespace = True
+def generate_dd_docs(app: Sphinx):
+    """Read IDSDef.xml and generate rst reference files.
 
-    def run(self) -> None:
-        rst = dedent(
-            """
-            .. _`ids reference`:
+    Generate rst files for: all IDSs, common utilities and identifiers.
+    """
+    logger.info("Generating DD documentation sources.")
+    etree = ElementTree.parse("../IDSDef.xml")
+    # Ensure output folders exist
+    for folder in ("ids", "util", "identifier"):
+        (Path("generated") / folder).mkdir(parents=True, exist_ok=True)
 
-            IDS reference
-            -------------
+    for ids in etree.iterfind("IDS"):
+        idsname = ids.get("name")
+        if not idsname:
+            raise RuntimeError("Empty IDS name!")
+        docfile = f"generated/ids/{idsname}.rst"
+        Path(docfile).write_text(ids2rst(ids))
 
-            .. toctree::
-                :name: dd-reference-toc
-                :glob:
-                :maxdepth: 1
+    for util in DOCUMENTED_UTILITIES:
+        node = etree.find(f"utilities/field[@name='{util}']")
+        if not node:
+            raise RuntimeError(f"Utility {util} does not exist in DD XML")
+        docfile = f"generated/util/{util}.rst"
+        Path(docfile).write_text(util2rst(node))
 
-                generated/ids/*
-
-            Common IDS structures reference
-            -------------------------------
-
-            .. toctree::
-                :name: dd-reference-toc-util
-                :glob:
-                :maxdepth: 1
-
-                generated/util/*
-
-            .. _`identifier reference`:
-
-            Identifier reference
-            --------------------
-
-            .. toctree::
-                :name: dd-reference-toc-identifiers
-                :glob:
-                :maxdepth: 1
-
-                generated/identifier/*
-            """
-        )
-        self.result = StringList()
-        for line in rst.splitlines():
-            self.result.append(line, *self.get_source_info())
-
-        # handle result
-        node = nodes.section()
-        # necessary so that the child nodes get the right source/line set
-        node.document = self.state.document
-        nested_parse_with_titles(self.state, self.result, node)
-        return node.children
+    # Find all ../*/*_identifier.xml files
+    for identifier in Path.cwd().parent.glob("*/*_identifier.xml"):
+        iden_tree = ElementTree.parse(identifier)
+        element = iden_tree.getroot()
+        docfile = f"generated/identifier/{identifier.stem}.rst"
+        fname = identifier.relative_to(identifier.parents[1])  # folder/file.xml
+        Path(docfile).write_text(identifier2rst(element, fname))
+    logger.info("Finished generating DD documentation sources.")
 
 
 def parse_documentation(text: str) -> str:
-    """Parse documentation string from a DD element."""
+    """Parse documentation string from a DD node.
+
+    Args:
+        text: documentation string (from field/@documentation attribute)
+    """
     # Escape special characters used by ReST as markup: *, `, |
     text = re.sub(r"([*`|])", r"\\\1", text)
     # Inline math
@@ -83,6 +68,11 @@ def parse_documentation(text: str) -> str:
 
 
 def link_to_coordinate(coordinate: str) -> str:
+    """Generate rst to link to a coordinate.
+
+    Args:
+        coordinate: coordinate string (from field/@coordinate<N>[_same_as])
+    """
     result = []
     for coor in coordinate.split(" OR "):
         if coor.startswith("1..."):
@@ -95,6 +85,7 @@ def link_to_coordinate(coordinate: str) -> str:
 
 
 def parse_lifecycle_status(field: ElementTree.Element) -> List[str]:
+    """Parse lifecycle status and return a list of rst source lines."""
     result = []
     lifecycle_status = field.get("lifecycle_status")
     lifecycle_version = field.get("lifecycle_version")
@@ -102,48 +93,18 @@ def parse_lifecycle_status(field: ElementTree.Element) -> List[str]:
 
     if lifecycle_status == "obsolescent":
         result.append(f".. deprecated:: {lifecycle_version}")
-    elif lifecycle_status == "alpha":
-        result.append(f".. versionadded:: {lifecycle_version}")
-        result.append(f"  Alpha since version {lifecycle_version}.")
-    elif lifecycle_status == "active":
-        result.append(f".. versionadded:: {lifecycle_version}")
-        result.append(f"  Active since version {lifecycle_version}.")
+    elif lifecycle_status in ("alpha", "active"):
+        status = f"lifecycle status `{lifecycle_status}`"
+        result.append(f".. versionadded:: {lifecycle_version} {status}")
+    elif lifecycle_status:
+        logger.warning("Unknown lifecycle status %s", lifecycle_status)
 
     if lifecycle_last_change:
         result.append(f".. versionchanged:: {lifecycle_last_change}")
-        result.append(f"  Last change occurred in version {lifecycle_last_change}.")
 
     if result:
         result.append("")
     return result
-
-
-def generate_dd_docs(app: Sphinx):
-    """Read IDSDef.xml and generate rst source files for all IDSs."""
-    etree = ElementTree.parse("../IDSDef.xml")
-    for ids in etree.iterfind("IDS"):
-        idsname = ids.get("name")
-        if not idsname:
-            raise RuntimeError("Empty IDS name!")
-        docfile = f"generated/ids/{idsname}.rst"
-        Path(docfile).parent.mkdir(parents=True, exist_ok=True)
-        Path(docfile).write_text(ids2rst(ids))
-
-    for util in DOCUMENTED_UTILITIES:
-        node = etree.find(f"utilities/field[@name='{util}']")
-        if not node:
-            raise RuntimeError(f"Utility {util} does not exist in DD XML")
-        docfile = f"generated/util/{util}.rst"
-        Path(docfile).parent.mkdir(parents=True, exist_ok=True)
-        Path(docfile).write_text(util2rst(node))
-
-    for identifier in Path.cwd().parent.glob("*/*identifier.xml"):
-        iden_tree = ElementTree.parse(identifier)
-        element = iden_tree.getroot()
-        docfile = f"generated/identifier/{identifier.stem}.rst"
-        Path(docfile).parent.mkdir(parents=True, exist_ok=True)
-        fname = identifier.relative_to(identifier.parents[1])  # folder/file.xml
-        Path(docfile).write_text(identifier2rst(element, fname))
 
 
 def util2rst(node: ElementTree.Element) -> str:
@@ -157,12 +118,13 @@ def util2rst(node: ElementTree.Element) -> str:
     result.append(f".. dd:util:: {name}")
     # TODO: options for utils?
     result.append("")
-    result.append(indent(parse_documentation(node.get("documentation")), "  "))
+    result.append(indent(parse_documentation(node.get("documentation")), INDENT))
     result.append("")
-    result.append(indent("\n".join(parse_lifecycle_status(node)), "  "))
+    result.append(indent("\n".join(parse_lifecycle_status(node)), INDENT))
     result.append(children2rst(node, 1))
     result.append("")
-    return "\n".join(result)
+    # Collapse triple or more newlines
+    return re.sub(r"[\n]{2,}", "\n\n", "\n".join(result))
 
 
 def ids2rst(ids: ElementTree.Element) -> str:
@@ -181,23 +143,33 @@ def ids2rst(ids: ElementTree.Element) -> str:
     result.append(f".. dd:ids:: {name}")
     # TODO: options for IDS
     result.append("")
-    result.append(indent(parse_documentation(ids.get("documentation")), "  "))
+    result.append(indent(parse_documentation(ids.get("documentation")), INDENT))
     result.append("")
-    result.append(indent("\n".join(parse_lifecycle_status(ids)), "  "))
+    result.append(indent("\n".join(parse_lifecycle_status(ids)), INDENT))
     result.append(children2rst(ids, 1))
     result.append("")
-    return "\n".join(result)
+    # Collapse triple or more newlines
+    return re.sub(r"[\n]{2,}", "\n\n", "\n".join(result))
 
 
 def field2rst(field: ElementTree.Element, has_error: bool, level: int) -> str:
-    """Convert an IDS Field element to rst documentation."""
+    """Convert an IDS Field element to rst documentation.
+
+    Args:
+        field: XML element of this field
+        has_error: True iff this field has corresponding error nodes
+        level: indentation level
+    """
+    # Link to common utilities that are documented elsewhere
     if field.get("structure_reference") in DOCUMENTED_UTILITIES:
         util = field.get("structure_reference")
-        assert len(util.split()) == 1, "structure_reference contains whitespace"
+        assert len(util.split()) == 1, "structure_reference may not contain whitespace"
         path = field.get("path_doc")
-        return f"{'  ' * level}.. dd:util-ref:: {path} {util}\n"
+        return indent(f".. dd:util-ref:: {path} {util}\n", INDENT * level)
+
     result = []
-    result.append(f"{'  ' * level}.. dd:node:: {field.get('path_doc')}")
+    result.append(f".. dd:node:: {field.get('path_doc')}")
+
     # options for nodes
     result.append(f":data_type: {field.get('data_type')}")
     if "type" in field.keys():
@@ -212,12 +184,6 @@ def field2rst(field: ElementTree.Element, has_error: bool, level: int) -> str:
     result.append(parse_documentation(field.get("documentation")))
     result.append("")
 
-    # Miscellaneous attributes
-    if "appendable_by_appender_actor" in field.keys():
-        appendable = field.get("appendable_by_appender_actor")
-        result.append(f":Appendable by appender actor: {appendable}")
-    result.append("")
-
     # Identifier
     if "doc_identifier" in field.keys():
         identifier = f":dd:identifier:`{field.get('doc_identifier')}`"
@@ -227,22 +193,28 @@ def field2rst(field: ElementTree.Element, has_error: bool, level: int) -> str:
         )
         result.append("")
 
+    # Miscellaneous attributes
+    if "appendable_by_appender_actor" in field.keys():
+        appendable = field.get("appendable_by_appender_actor")
+        result.append(f":Appendable by appender actor: {appendable}")
+    result.append("")
+
     # Coordinates
     coordinates_csv = []
     for i in range(6):
         coordinate = field.get(f"coordinate{i+1}")
         if not coordinate:
             break
-        # TODO: create reference
-        csv_line = f"  {i+1},{link_to_coordinate(coordinate)}"
         coordinate_same_as = field.get(f"coordinate{i+1}_same_as")
+        same_as = ""
         if coordinate_same_as:
-            csv_line += f" (same as {link_to_coordinate(coordinate_same_as)})"
-        coordinates_csv.append(csv_line)
+            same_as += f" (same as {link_to_coordinate(coordinate_same_as)})"
+        coordinates_csv.append(f' {i+1},"{link_to_coordinate(coordinate)}{same_as}"')
+
     if coordinates_csv:
         result.append(".. csv-table::")
-        result.append("  :class: dd-coordinates")
-        result.append("  :header: ,:ref:`Coordinate <coordinates>`")
+        result.append(" :class: dd-coordinates")
+        result.append(" :header: ,:ref:`Coordinate <coordinates>`")
         result.append("")
         result.extend(coordinates_csv)
         result.append("")
@@ -269,17 +241,34 @@ def field2rst(field: ElementTree.Element, has_error: bool, level: int) -> str:
     # Lifecycle information
     result.extend(parse_lifecycle_status(field))
     level += 1
-    return f"\n{'  '*level}".join(result) + "\n" + children2rst(field, level)
+    result = indent("\n".join(result), INDENT * level)
+    result = result[len(INDENT) :]  # Indent first line one level lower
+    # Add documentation for children
+    result += "\n" + children2rst(field, level)
+    return result
 
 
 def children2rst(element: ElementTree.Element, level: int) -> str:
+    """Convert all children of an IDS element to rst documentation.
+
+    Args:
+        element: XML element of this field
+        level: indentation level
+    """
     children = {field.get("name"): field for field in element.iterfind("field")}
+
+    # Check which fields have error nodes, and skip those error nodes
     has_error = set()
     skip = set()
     for fieldname in children:
-        if "_error" in fieldname:
-            skip.add(fieldname)
-            has_error.add(fieldname[: fieldname.find("_error")])
+        if fieldname in skip:
+            continue
+        for error_suffix in ("_error_upper", "_error_lower", "_error_index"):
+            error_name = fieldname + error_suffix
+            if error_name in children:
+                has_error.add(fieldname)
+                skip.add(error_name)
+
     return "\n".join(
         field2rst(field, fieldname in has_error, level)
         for fieldname, field in children.items()
@@ -287,9 +276,17 @@ def children2rst(element: ElementTree.Element, level: int) -> str:
     )
 
 
-def identifier2rst(element: ElementTree.Element, fname: str) -> str:
+def identifier2rst(element: ElementTree.Element, fname: Path) -> str:
+    """Generate rst documentation for an identifier.
+
+    Args:
+        element: root element (constants tag) of _identifier.xml
+        fname: Path consisting of the parent folder and filename of the identifier, for
+            example ``Path("utilities/neutrals_identifier.xml")``
+    """
+    # Construct CSV
     csv_items = "\n".join(
-        f'"{ele.get("name")}", "{ele.text}", "{ele.get("description")}"'
+        f'"{ele.get("name")}","{ele.text}","{ele.get("description")}"'
         for ele in element.iterfind("int")
     )
 
@@ -300,15 +297,17 @@ def identifier2rst(element: ElementTree.Element, fname: str) -> str:
     result.append(title)
     result.append("=" * len(title))
     result.append("")
-    result.append(".. csv-table::")
-    result.append('  :header: "Name","Index","Description"')
+    for line in element.find("header").text.splitlines():
+        result.append(line.strip())
     result.append("")
-    result.append(indent(csv_items, '    '))
+    result.append(".. csv-table::")
+    result.append(f'{INDENT}:header: "Name","Index","Description"')
+    result.append("")
+    result.append(indent(csv_items, INDENT))
     result.append("")
     return "\n".join(result)
 
 
 def setup(app: Sphinx) -> Dict[str, Any]:
     app.setup_extension("sphinx_dd_extension.dd_domain")
-    app.add_directive_to_domain("dd", "autodoc", DDAutoDoc)
     app.connect("builder-inited", generate_dd_docs)
