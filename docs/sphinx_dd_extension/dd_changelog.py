197d687e39024062b5a4eba91cce79f507447927
Yannick de Jong <ydejong@ignitioncomputing.com>
2023-09-26 14:40:32 +0200

    Generate IDS changes documentation

--- docs/sphinx_dd_extension/dd_changelog.py
+++ docs/sphinx_dd_extension/dd_changelog.py
@@ -10,6 +10,12 @@ from git import Repo, Tag
 import json
 import re
 
+from imaspy import IDSFactory
+from imaspy.dd_zip import dd_xml_versions
+from imaspy.ids_convert import dd_version_map_from_factories
+
+import os
+
 from sphinx.application import Sphinx
 from sphinx.util import logging
 
@@ -56,14 +62,22 @@ def replace_imas_jira(text: str):
     )
 
 
-def generate_dd_changelog(app: Sphinx):
+def get_tags():
+    repo = Repo("..")
+
+    tags = sort_tags(repo.tags)
+
+    return tags
+
+
+def generate_git_changelog(app: Sphinx):
     """Generate a changelog using git pull requests"""
-    logger.info("Generating DD changelog sources.")
+    logger.info("Generating DD git changelog sources.")
 
     # Ensure output folders exist
     (Path("generated/changelog")).mkdir(parents=True, exist_ok=True)
 
-    docfile = Path("generated/changelog/changelog.rst")
+    docfile = Path("generated/changelog/git.rst")
 
     docfile.unlink(True)
 
@@ -139,7 +153,180 @@ def generate_dd_changelog(app: Sphinx):
     logger.info("Finished generating DD changelog sources.")
 
 
+def heading(s: str, style="-"):
+    return f"{s}\n{style*len(s)}\n\n"
+
+
+def ids_changes(ids_name: str, from_factory, to_factory):
+    added: list[str] = []
+    removed: list[str] = []
+    renamed: list[tuple[str, str]] = []
+    # retyped: list[tuple[str, str, str]]
+    version_map, old = dd_version_map_from_factories(ids_name, from_factory, to_factory)
+    for f, t in version_map.old_to_new.path.items():
+        if f == t:
+            pass
+        if f in version_map.old_to_new.type_change:
+            pass
+        elif t is None:
+            removed.append(f)
+        else:
+            renamed.append((f, t))
+
+    for f, t in version_map.new_to_old.path.items():
+        if t is None and f not in version_map.new_to_old.type_change:
+            added.append(f)
+    return added, removed, renamed
+
+
+def indent(s, i):
+    output = ""
+    for line in s.split("\n"):
+        if len(line) > 0:
+            output += f"{' '*i}{line}\n"
+        else:
+            output += "\n"
+    return output
+
+
+class TreeNode:
+    def __init__(self, name="root_node"):
+        self.children: dict[str, TreeNode] = {}
+        self.name = name
+
+    def add_path(self, path: str, postfix=""):
+        split_path = path.split("/", maxsplit=1)
+        if len(split_path) == 2:
+            name, remaining_path = split_path
+            self.children.setdefault(name, TreeNode(name))
+            self.children[name].add_path(remaining_path, postfix)
+        else:
+            name = split_path[0]
+            self.children.setdefault(name, TreeNode(name + postfix))
+
+    def __repr__(self) -> str:
+        return self.__str__()
+
+    def _str(self, output="", prefix="", child_prefix=""):
+        output = prefix + self.name + "\n"
+        if self.name == "root_node":
+            output = ""
+        for i, child in enumerate(self.children.values()):
+            if i == len(self.children) - 1:
+                gen_prefix = child_prefix + "└─"
+                gen_child_prefix = child_prefix + "  "
+            else:
+                gen_prefix = child_prefix + "├─"
+                gen_child_prefix = child_prefix + "│ "
+            output += child._str(output, gen_prefix, child_prefix=gen_child_prefix)
+        return output
+
+    def __str__(self):
+        return self._str()
+
+
+def get_relative_path(a: str, b: str):
+    return os.path.relpath(b, a)
+
+
+def to_tree_with_postfix(a: list[str], root_name="root"):
+    output = ".. code-block:: \n\n"
+    t = TreeNode()
+    for i, j in sorted(a):
+        t.add_path(i, f" → {'/'.join(get_relative_path(i, j).split('/')[1:])}")
+    output += indent(str(t), 4)
+    return output
+
+
+def to_tree(a: list[str], root_name="root"):
+    output = ".. code-block:: \n\n"
+    t = TreeNode()
+    a.sort()
+    for i in a:
+        t.add_path(i)
+    output += indent(str(t), 4)
+    return output
+
+
+def format_renamed(renamed):
+    output = ".. code-block:: \n\n"
+    for f, t in renamed:
+        output += indent(f"{f} -> {t}", 4)
+    return output
+
+
+def generate_dd_changelog(app: Sphinx):
+    logger.info("Generating DD ids changelog sources.")
+
+    # Ensure output folders exist
+    (Path("generated/changelog/ids_changes")).mkdir(parents=True, exist_ok=True)
+
+    my_ids_xml = "../IDSDef.xml"
+
+    factory = IDSFactory("3.39.0")
+
+    print(factory.version)
+
+    versions = [
+        x.name
+        for x in get_tags()
+        if x.name != factory.version and x.name in dd_xml_versions()
+    ]
+
+    docfile = Path("generated/changelog/ids.rst")
+
+    docfile.unlink(True)
+
+    output = "IDS\n===\n\n"
+
+    output += ".. toctree::\n   :maxdepth: 1\n   :caption: IDS versions\n\n"
+
+    for version in versions:
+        version_docfile = Path(f"generated/changelog/ids_changes/{version}.rst")
+
+        version_docfile.unlink(True)
+
+        text = ""
+
+        from_factory = IDSFactory(version)
+
+        added_ids = set(factory).difference(from_factory)
+        removed_ids = set(from_factory).difference(factory)
+
+        text += heading(version, style="=")
+        for i in added_ids:
+            text += heading(f"(green)+{i}")
+        for i in removed_ids:
+            text += heading(f"(red)-{i}")
+
+        for i in set(factory).intersection(set(from_factory)):
+            added, removed, renamed = ids_changes(i, from_factory, factory)
+            if len(added) > 0 or len(removed) > 0 or len(renamed) > 0:
+                text += heading(i)
+            if len(added) > 0:
+                text += heading("Added", "*")
+                text += to_tree(added, i)
+                text += "\n"
+            if len(removed) > 0:
+                text += heading("Removed", "*")
+                text += to_tree(removed, i)
+                text += "\n"
+            if len(renamed) > 0:
+                text += heading("Renamed", "*")
+                text += to_tree_with_postfix(renamed)
+                text += "\n"
+
+        with open(version_docfile, "w") as f:
+            f.write(text)
+
+        output += f"   ids_changes/{version}\n"
+
+    with open(docfile, "w") as f:
+        f.write(output)
+
+
 def setup(app: Sphinx) -> Dict[str, Any]:
+    app.connect("builder-inited", generate_git_changelog)
     app.connect("builder-inited", generate_dd_changelog)
     return {
         "version": "0.1",
