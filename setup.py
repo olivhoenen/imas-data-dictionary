fd446b284fbdfcbf8237ca367d4a459a95c435d6
Sawantdesai Prasad parshuram EXT <Prasadparshuram.Sawantdesai@iter.org>
2024-07-25 16:03:12 +0200

    fixed configuration for data_files

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from setuptools import setup
 from setuptools_scm import get_version
 import glob
 import os
-import pathlib
 import sys
 
 sys.path.append(str(Path(__file__).parent.resolve()))
@@ -32,49 +31,42 @@ from install import (
     install_utilities_files,
 )
 
-current_directory = pathlib.Path(__file__).parent.resolve()
-# long_description = (current_directory / "README.md").read_text(encoding="utf-8")
+if __name__ == "__main__":
+    # Generate
+    generate_dd_data_dictionary()
+    generate_html_documentation()
+    generate_sphinx_documentation()
+    generate_ids_cocos_transformations_symbolic_table()
+    generate_idsnames()
+    generate_dd_data_dictionary_validation()
 
-# Generate
-generate_dd_data_dictionary()
-generate_html_documentation()
-generate_sphinx_documentation()
-generate_ids_cocos_transformations_symbolic_table()
-generate_idsnames()
-generate_dd_data_dictionary_validation()
+    # install
+    install_html_files()
+    install_sphinx_files()
+    install_css_files()
+    install_js_files()
+    install_img_files()
+    install_cocos_csv_files()
+    install_ids_files()
+    install_dd_files()
+    install_utilities_files()
+    create_idsdef_symlink()
+    copy_utilities()
+    install_identifiers_files()
 
-# install
-install_html_files()
-install_sphinx_files()
-install_css_files()
-install_js_files()
-install_img_files()
-install_cocos_csv_files()
-install_ids_files()
-install_dd_files()
-install_utilities_files()
-create_idsdef_symlink()
-copy_utilities()
-install_identifiers_files()
-
-# stores include and share folder in root python path while installing
-paths = []
-version = get_version()
-if os.path.exists("install"):
-    for path, directories, filenames in os.walk("install"):
-        paths.append(
-            (path.replace("install", "dd_" + version), glob.glob(path + "/*.*"))
+    # stores include and share folder in root python path while installing
+    paths = []
+    version = get_version()
+    if os.path.exists("install"):
+        for path, directories, filenames in os.walk("install"):
+            paths.append(
+                (path.replace("install", "dd_" + version), glob.glob(path + "/*.*"))
+            )
+    else:
+        raise Exception(
+            "Couldn't find IDSDef.xml, Can not install data dictionary python package"
         )
-else:
-    raise Exception(
-        "Couldn't find IDSDef.xml, Can not install data dictionary python package"
-    )
 
-setup(
-    use_scm_version=True,
-    data_files=paths,
-    scripts=[
-        "scripts/dd_doc",
-        "scripts/dd_doclegacy"
-    ],
-)
+    setup(
+        data_files=paths,
+    )
